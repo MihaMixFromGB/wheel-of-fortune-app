d4fa393ec3c8c90f97f75440d75e09c5e0e11dd1
MihaMixFromGB <mihamix@yandex.ru>
2023-02-26 22:03:00 +0700

    Add Facebook API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import bridge from "@vkontakte/vk-bridge";
+//import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 import "./style.css";
 
-// Init VK  Mini App
-bridge.send("VKWebAppInit");
+// Init VK Mini App
+//bridge.send("VKWebAppInit");
+
+// FBInstant.player будет инициализирован после реализации promise от FBInstant.startGameAsync
+// Требуется синхронизация с useEffect в App
+// window.onload = function () {
+//   // Init Facebook Instant Games SDK
+//   FBInstant.initializeAsync().then(
+//     // This indicates that the game has finished initial loading and is ready to start
+//     FBInstant.startGameAsync().then(() => {
+//       console.log(
+//         "userId from 'startGameAsync' promise",
+//         FBInstant.player.getID()
+//       );
+//     })
+//   );
+// };
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // if (process.env.NODE_ENV === "development") {
